d0827b3aa071071bbd29b18e0dc9bf2d1eef50e8
Chris Engelhart <engleman@gmail.com>
2021-04-22 08:27:20 -0400

    Testing multiple updates...

--- cursessim.py
+++ cursessim.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Simulator of splitflap display using Curses"""
+
 import time
 import curses
 
@@ -10,45 +12,58 @@ current = ['                ',
 
 
 def update(line=0, text=''):
+    """Update display with a new string"""
     future[line] = '{:^16}'.format(text.upper())
 
 def steps(win):
+    """Do single flap of all charachters"""
     for i in range(2):
         for j in range(16):
             if future[i][j] != current[i][j]:
                 newch = chr(ord(current[i][j]) + 1)
-                if newch == '[':
-                    newch == ' '
+                if newch == '`':
+                    newch = ' '
                 tmp = list(current[i])
                 tmp[j] = newch
                 current[i] = ''.join(tmp)
-                win.addch(i+1,j+1,newch)
+                win.addch(i+1, j+1, newch)
 
 def main(stdscr):
+    """Main curses function"""
     stdscr.clear()
     curses.curs_set(False)
     assert curses.LINES > 3
     assert curses.COLS > 17
     win = curses.newwin(4, 18, 0, 0)
     win.box()
-    #win.addstr(1, 1, 'hello world')
-    #win.addch(2, 1, 'a')
     update(text='hello world')
-    for i in range(256):
+    for _ in range(64):
         steps(win)
         win.refresh()
-        time.sleep(1)
-    win.addch(2, 1, 'b')
+        time.sleep(0.125)
+    win.refresh()
+    win.getkey()
+    update(line=1, text='hello world')
+    for _ in range(64):
+        steps(win)
+        win.refresh()
+        time.sleep(0.125)
+    update(line=0, text='goodbye world')
+    for _ in range(64):
+        steps(win)
+        win.refresh()
+        time.sleep(0.125)
     win.refresh()
     win.getkey()
 
+
 curses.wrapper(main)
+
 
 #stdscr = curses.initscr()
 #curses.noecho()
 #curses.cbreak()
 #stdscr.keypad(True)
-
 
 #curses.nocbreak()
 #stdscr.keypad(False)
