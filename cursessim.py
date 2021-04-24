93df8ad97d2a722110eef87046d141791f72b28b
Chris Engelhart <engleman@gmail.com>
2021-04-24 12:50:57 -0400

    Made SplitFlap class
    Closes #1

--- cursessim.py
+++ cursessim.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 """Simulator of splitflap display using Curses"""
 
 
@@ -8,48 +7,64 @@ import time
 import curses
 
 
-future =  ['                ',
-           '                ']
-current = ['                ',
-           '                ']
+class SplitFlap:
+    """A splitflap display"""
 
+    def __init__(self, win):
+        self.win = win
+        self.future =  ['                ',
+                        '                ']
+        self.current = ['                ',
+                        '                ']
 
-def linestatus():
-    """Check to see if lines are up to date or still updating"""
-    overall = True
-    individ = []
-    for idx in range(len(current)):
-        if future[idx] == current[idx]:
-            individ.append(True)
-        else:
-            individ.append(False)
-            overall = False
-    return overall, individ
+    def linestatus(self):
+        """Check to see if lines are up to date or still updating"""
+        overall = True
+        individ = []
+        for idx in range(len(self.current)):
+            if self.future[idx] == self.current[idx]:
+                individ.append(True)
+            else:
+                individ.append(False)
+                overall = False
+        return overall, individ
 
-def update(line=0, text=''):
-    """Update display with a new string"""
-    future[line] = '{:^16}'.format(text.upper())
+    def update(self, line=0, text=''):
+        """Update display with a new string"""
+        self.future[line] = '{:^16}'.format(text.upper())
 
-def steps(win):
-    """Do single flap of all charachters"""
-    for i in range(2):
-        for j in range(16):
-            if future[i][j] != current[i][j]:
-                newch = chr(ord(current[i][j]) + 1)
-                if newch == '`':
-                    newch = ' '
-                tmp = list(current[i])
-                tmp[j] = newch
-                current[i] = ''.join(tmp)
-                win.addch(i+1, j+1, newch)
+    def steps(self):
+        """Do single flap of all charachters"""
+        for i in range(2):
+            for j in range(16):
+                if self.future[i][j] != self.current[i][j]:
+                    newch = chr(ord(self.current[i][j]) + 1)
+                    if newch == '`':
+                        newch = ' '
+                    tmp = list(self.current[i])
+                    tmp[j] = newch
+                    self.current[i] = ''.join(tmp)
+                    self.win.addch(i+1, j+1, newch)
 
-def gountilfinished(win):
-    """Step through flaps until display is accurate"""
-    while not linestatus()[0]:
-        steps(win)
-        win.refresh()
-        time.sleep(0.125)
-    win.refresh()
+    def gountilfinished(self):
+        """Step through flaps until display is accurate"""
+        while not self.linestatus()[0]:
+            self.steps()
+            self.win.refresh()
+            time.sleep(0.125)
+        self.win.refresh()
+
+    def addtotop(self, line):
+        """Add a line of text to the top of the screen
+        Moving down all lines beneath it one by one
+        """
+        for idx in reversed(range(len(self.future))):
+            if self.future[idx].strip() and idx != len(self.future):
+                self.future[idx + 1] = self.future[idx]
+            self.gountilfinished()
+        self.update(text=line)
+        self.gountilfinished()
+
 
 def initscr(stdscr):
     """Initiate screen and basic display"""
@@ -61,28 +76,19 @@ def initscr(stdscr):
     win.box()
     return win
 
-def addtotop(line, win):
-    """Add a line of text to the top of the screen
-    Moving down all lines beneath it one by one
-    """
-    for idx in reversed(range(len(future))):
-        if future[idx].strip() and idx != len(future):
-            future[idx + 1] = future[idx]
-            gountilfinished(win)
-    update(text=line)
-    gountilfinished(win)
-
 def main(stdscr):
     """Main curses function"""
     win = initscr(stdscr)
-    update(text='hello world')
-    gountilfinished(win)
+    mysf = SplitFlap(win)
+    mysf.update(text='hello world')
+    mysf.gountilfinished()
     win.getkey()
-    addtotop('goodbye world', win)
+    mysf.addtotop('goodbye world')
     win.getkey()
 
 
-curses.wrapper(main)
+if __name__ == "__main__":
+    curses.wrapper(main)
 
 
 #stdscr = curses.initscr()
