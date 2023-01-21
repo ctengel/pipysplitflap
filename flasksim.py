cfda4f803e206d7aa5780f91c07ef9471e393601
Chris Engelhart <engleman@gmail.com>
2023-01-21 07:06:17 -0500

    Added Thruway and NWS sources
    Part of #9

--- flasksim.py
+++ flasksim.py
@@ -1,5 +1,10 @@
 import random
+import xml.etree.ElementTree as ET
 from flask import Flask, render_template
+import requests
+
+THRUWAY = True
+WEATHER = "NY"
 
 app = Flask(__name__)
 
@@ -7,7 +12,24 @@ app = Flask(__name__)
 def main():
     return render_template('ticker.html')
 
+def get_thruway():
+    tree = ET.fromstring(requests.get("https://www.thruway.ny.gov/xml/netdata/dmsstatus.xml").content)
+    signs = {x.attrib['sign-text-1'].replace('~', ' ').strip() + ' ' + x.attrib['sign-text-2'].replace('~', ' ').strip() for x in tree.findall('dms')}
+    signs.remove(' ')
+    return [{'text': x.upper()} for x in signs]
+
+def get_weather(state):
+    weather = requests.get("https://api.weather.gov/alerts/active", params={'area': state}).json()
+    events = {x['properties']['event'] for x in weather['features']}
+    return [{'text': x.upper()} for x in events]
+
+
 @app.route("/board")
 def render_board():
-    return {'messages': [{'text': random.choice(['HELLO', 'WORLD'])}]}
+    messages = []
+    if THRUWAY:
+        messages = messages + get_thruway()
+    if WEATHER:
+        messages = messages + get_weather(WEATHER)
+    return {'messages': messages}
 
