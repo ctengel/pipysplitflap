782f0a13e5caa33acce2a93e298d3c1f01a9bab5
Chris Engelhart <engleman@gmail.com>
2023-01-23 07:05:37 -0500

    More focused NWS and Thruway alerts
    NWS now based on county, not state
    Thruway filtered to region(s)
    
    Part of #9

--- flasksim.py
+++ flasksim.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 import requests
 import feedparser
 
-THRUWAY = True
-WEATHER = "NY"
+THRUWAY = ['NY', 'MH']
+WEATHER = "NYC087"
 RSS = ['http://feeds.bbci.co.uk/news/rss.xml?edition=us',
        'https://www.ncregister.com/feeds/general-news.xml']#,
 #       'http://rss.slashdot.org/Slashdot/slashdotMainatom']
@@ -16,14 +16,14 @@ def main():
     #print(request.headers)
     return render_template('flapper.html')
 
-def get_thruway():
+def get_thruway(regions):
     tree = ET.fromstring(requests.get("https://www.thruway.ny.gov/xml/netdata/dmsstatus.xml").content)
-    signs = {x.attrib['sign-text-1'].replace('~', ' ').strip() + ' ' + x.attrib['sign-text-2'].replace('~', ' ').strip() for x in tree.findall('dms')}
+    signs = {x.attrib['sign-text-1'].replace('~', ' ').strip() + ' ' + x.attrib['sign-text-2'].replace('~', ' ').strip() for x in tree.findall('dms') if x.attrib['region'] in regions}
     signs.remove(' ')
     return [{'text': x.upper()} for x in signs]
 
 def get_weather(state):
-    weather = requests.get("https://api.weather.gov/alerts/active", params={'area': state}).json()
+    weather = requests.get("https://api.weather.gov/alerts/active", params={'zone': state}).json()
     events = {x['properties']['event'] for x in weather['features']}
     return [{'text': x.upper()} for x in events]
 
@@ -39,7 +39,7 @@ def get_rss(urls):
 def render_board():
     messages = []
     if THRUWAY:
-        messages = messages + get_thruway()
+        messages = messages + get_thruway(THRUWAY)
     if WEATHER:
         messages = messages + get_weather(WEATHER)
     if RSS:
